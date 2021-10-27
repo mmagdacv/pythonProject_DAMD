360f4d83b231b51a8100a22c6c6dc856d85b23ff
María Magdalena Carril <mmcarril@ATEIS.LOCAL>
2021-10-27 18:12:46 +0200

    prueba

---
+++ ProyUno/events.py
@@ -0,0 +1,23 @@
+import var, sys
+
+class Eventos():
+
+    '''
+    def Saludo():
+        try:
+
+            var.ui.btnAceptar.setText('OK')
+        except Exception as error:
+            print('Error: %s ' % str(error))
+
+    '''
+
+    def Salir():
+        try:
+
+            sys.exit()
+        except Exception as error:
+            print('Error: %s ' % str(error))
+
+
+
---
+++ ProyUno/main.py
@@ -0,0 +1,26 @@
+from ventana import *
+
+import sys, var, events
+
+
+class Main(QtWidgets.QMainWindow):
+
+    def __init__(self):
+        super(Main, self).__init__()
+        var.ui = Ui_MainWindow()
+        var.ui.setupUi(self)
+
+        '''        
+        var.ui.btnAceptar.clicked.connect(events.Eventos.Saludo)
+        '''
+
+        var.ui.actionSalir.triggered.connect(events.Eventos.Salir)
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication([])
+    window = Main()
+    window.show()
+    sys.exit(app.exec())
+
+
